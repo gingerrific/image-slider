fa6a19ca4884a99082966ee221628fa0e11e9d46
Josh Date <date0010@me.com>
2014-05-28 23:46:19 -0400

    converted full slider into constructor. added funcitonal tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,89 +1,119 @@
 'use strict';
  
  
+	var i = 0;
 var imagesArray = ["https://s3.amazonaws.com/ksr/projects/660047/photo-main.jpg?1397831981", "http://supergiantgames.com/site/wp-content/uploads/Transistor_Wallpaper_1920x1080.jpg", "http://cloud-4.steampowered.com/ugc/615043645846589420/E237625134C7E37487EC553109818982CE3FD0FD/", "http://static2.cdn.ubi.com/emea/gamesites/TCTD/wallpapers/TheDivision_Wallpapers_1024x768.jpg", "http://thekoalition.com/images/2013/08/bell.jpg"];
  
-function ImageSlide () {
-
-} 
-
-
-ImageSlide.prototype.renderContainer = function (images) {
-	if ($.isArray(images) && images !== []){	
-	  // error checks for array type
-		var max = images.length
-		// sets the display width of the div to accomodate varying width based on array length
-		$('.image-slider').css('width',(max*100)+'%');
-  	}
-	else {
-		throw new Error("ImageParse requires a non-empty array as argument");
+function ImageSlide (array, firstImagePos, delay) {
+	// local constructor variable inits
+	var intervalTime;
+	var intervalName;
+	// if a delay argument is passed, set this at the interval timing, otherwise use 2400 as default
+	if (delay) {
+		intervalTime = delay; 
 	}
-}
-  
-ImageSlide.prototype.showImages = function (images) {
-	images.map(function (x) {
-	  // appends the contents of the .image-frame div into the dom for each index of the array (could posisbly be split off)
+	else {
+		intervalTime = 2400;
+	}
+	
+	//render container property
+	this.renderContainer = function () {
+		 // error checks for array type
+		if (!$.isArray(array)) {
+			throw new Error("ImageParse requires a non-empty array as argument");
+		}
+		else if (array <= 0) {
+			throw new Error ("The argument array is empty.");
+		}
+		else {
+			var max = array.length;
+			// sets the display width of the div to accomodate varying width based on array length
+			$('.image-slider').css('width',(max*100)+'%');
+		}
+	};
+	//re-arranges the array based on index value provided
+	this.order = function () {
+		if(firstImagePos) {
+			return (array.slice(firstImagePos)).concat(array.slice(0,firstImagePos));
+			}
+		else {
+			return array;
+		}	
+	};
+	// adds the necessary html elements based on an array's length and assigns the images to the background css property
+	this.sliderView = function () {
+		(this.order()).forEach(function (x) {
+		// appends the contents of the .image-frame div into the dom for each index of the array (could posisbly be split off)
 		$('.image-slider').append($('.image-frame').text());
 		// the most recent div will have its background-image changed to the formatted array index
 		$('.image-display').last().css('background', 'url(' + x + ') center / cover no-repeat');
-	})
-}
- 
- 
-var i = 0;
-var interval;
-// inter function that calls the animation, but also loops
-ImageSlide.prototype.imageMove = function () {
-		interval = setInterval(function () {
-		moveLeft();
+		});
+	};
+
+	// animate the slider moving left the width of an image
+	this.imageSlide = function () {
+		return $('.image-slider').css({'margin-left': '-=392px', 'transition':'all 1.6s linear'})
+	}
+
+	// interval function that will call the imageSlide animation every set interval (parameter or default of 2400)
+	// if the counter hits the array length - 1, it will reset itself back to 0 and also move the image slider to 0.
+	var that = this;	
+	this.changeImage = setInterval(function () {
+						that.imageSlide();
+						i += 1;
+							  // when the counter hits the end of the array (in this case) return to one. need to set to index.length instead of '6'
+							  // also resets the image slider to the first image
+							if (i >= array.length) {
+								i=0;
+								$('.image-slider').css({'margin-left': '0px', 'transition': 'none'})
+							}
+						}, intervalTime);
+
+	this.stopControl = function () {
+		clearInterval(this.changeImage);
+	}
+
+	this.nextImage = function () {
 		i += 1;
-		
-		if (i == 6) {
-		  // when the counter hits the end of the array (in this case) return to one. need to set to index.length instead of '6'
-		  // also resets the image slider to the first image
-			i=1;
-			$('.image-slider').animate({'margin-left': '0px', 'transition': 'none'});
-		}
- 
-	}, 2400);
- 
-}
-// animate the slider moving left the width of an image
-function moveLeft () {
-		$('.image-slider').css({'margin-left': '-=392px', 'transition':'all 1.6s linear'});	
-}
- 
-// 'controls' - allows the slider to be stopped, restarted and advanced
-$('.stop').click(function () {
-	$('.image-slider').stop();
-	clearInterval(interval);
-})
- 
-$('.play').click(function () {
-	var slideShow = new ImageSlide();
-	slideShow.renderContainer(imagesArray);
-	slideShow.showImages(imagesArray);
-	slideShow.imageMove();
-})
- 
-$('.previous').click(function () {
-	clearInterval(interval);
-	$('.image-slider').css({'margin-left': '+=392px'});
-	i -=1;	
-	if (i == -1) {
-		$('.image-slider').stop();
 		$('.image-slider').css({'margin-left': '-=392px'});
-		i = 1;
-	}
-})
- 
-$('.next').click(function () {
-	clearInterval(interval);
-	$('.image-slider').css({'margin-left': '-=392px'});	
-	i += 1;
-	if (i == 7) {
-		$('.image-slider').stop();
+		if (i >= array.length) {
+			$('.image-slider').stop();
+			var tooFar = (array.length-1)*-392
+			$('.image-slider').css({'margin-left': tooFar+'px'});
+			i = array.length;
+		}
+	}	
+
+	this.previousImage = function () {
+		i -=1;
 		$('.image-slider').css({'margin-left': '+=392px'});
-		i = 5;
+		if (i <= -1) {
+			$('.image-slider').css({'margin-left': '0px'});
+			i = 0;
+		}
 	}
-})
+}
+
+
+var slideShow = new ImageSlide(imagesArray, 0);
+slideShow.renderContainer();
+slideShow.sliderView();
+ 
+// // 'controls' - allows the slider to be stopped, restarted and advanced
+$('.stop').click(function () {
+	slideShow.stopControl();
+});
+
+$('.play').click(function () {
+	return slideShow = new ImageSlide(imagesArray, 0);
+});
+
+$('.previous').click(function () {
+	slideShow.previousImage();
+});
+
+$('.next').click(function () {
+	slideShow.nextImage();
+});
+
+ 
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,21 +3,78 @@
 (function () {
     'use strict';
     describe('ImageSlide Constructor', function () {
-		it('should correctly respond to its methods', function () {
-			var quickSlide = new ImageSlide();
+		
+		it('should correctly respond to its methods', function (done) {
+			var quickSlide = new ImageSlide(imagesArray);
 			expect(quickSlide).to.respondTo('renderContainer');
-			expect(quickSlide).to.respondTo('showImages');
-			expect(quickSlide).to.respondTo('imageMove');
+			expect(quickSlide).to.respondTo('sliderView');
+			expect(quickSlide).to.respondTo('order');
+			done();
 		});
 
-		it('should throw when given non array args', function () {
-        	
-			var badImage = function () {
-				var coolSlides = new ImageSlide()
-				coolSlides.renderContainer("cool")
+		it('should throw when given a non array agument', function () {
+
+			var badSlide = function () {
+				var coolSlides = new ImageSlide();
+				coolSlides.renderContainer("cool");
 			};
           
-			expect(badImage).to.throw(Error);
+			expect(badSlide).to.throw(Error);
+        });
+
+		it('should throw when given an empty array argument', function () {
+
+		var badSlide = function () {
+			var coolSlides = new ImageSlide();
+			return coolSlides.renderContainer([]);
+		};
+      
+		expect(badSlide).to.throw(Error);
+        });
+    });
+
+    describe('ImageSlide constructor instances', function () {
+		var imageArray2 = ["http://www.gameranx.com/img/14-Mar/luftrausers.jpg", "http://www.dealspwn.com/writer/wordpress/wp-content/uploads/2014/01/nidhogg11-540x338.jpg"];
+        
+        it('should accept any array of images for display in the slider', function () {
+
+			var customImageSlide = new ImageSlide(imageArray2);
+
+			expect(customImageSlide.order()[0]).to.equal('http://www.gameranx.com/img/14-Mar/luftrausers.jpg');
+        });
+
+        it('should accept and correctly apply a delay as an argument', function (done) {
+        	setTimeout(10000)
+			var customImageSlide = new ImageSlide(imageArray2, 4000);
+
+			setTimeout(function () {	
+				expect($('.image-slider').css('margin-left')).to.equal(-392+'px');
+			},4250)
+			done();
+
+        });
+
+        it('should correctly reorder an array based on the position passed as an argument', function () {
+
+			var customStartSlide = new ImageSlide(imageArray2, 1);
+			expect(customStartSlide.order()[0]).to.equal('http://www.dealspwn.com/writer/wordpress/wp-content/uploads/2014/01/nidhogg11-540x338.jpg');
+        });
+
+        it('should display the correct image when a position/index is passed as an argument', function () {
+        	$('.image-slider').html('');
+			var customStartSlide = new ImageSlide(imageArray2, 1);
+			customStartSlide.renderContainer();
+			customStartSlide.order();
+			customStartSlide.sliderView();
+
+			expect($('.image-display').first().css('background')).to.have.string('http://www.dealspwn.com/writer/wordpress/wp-content/uploads/2014/01/nidhogg11-540x338.jpg');
+        });
+
+        it('should be able to stop intervals called on itself', function () {
+			var quickSlideShow = new ImageSlide(imagesArray);
+			quickSlideShow.stopControl();
+
+			// expect()
         });
     });
 
@@ -26,24 +83,22 @@
 
         it('should correctly adjust the slider container size', function () {
 
-			var imageExample = new ImageSlide();
-			imageExample.renderContainer([1,2,3,4,5])
-        	// works in practice
-			expect($('.image-slider').css('width')).to.equal("500%");
+			var imageExample = new ImageSlide([1,2,3,4,5]);
+			imageExample.renderContainer();
+
+			expect($('.image-slider').css('width')).to.equal(5*$(window).width()+'px');
         });
     });
 
     describe('Image view', function () {
 		it('should keep track of its position in the loop', function () {
 
-			var imageExample = new ImageSlide();
-			imageExample.renderContainer(imagesArray);
-			imageExample.showImages(imagesArray);
+			var imageExample = new ImageSlide(imagesArray);
 			$('.next').click();
-			$('.next').click();
-			// works in practice
+
 			expect(i).to.equal(1);
-    	});
+
+		});
     });
 
 })();
